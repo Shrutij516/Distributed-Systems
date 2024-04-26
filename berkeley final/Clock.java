17145a6d59251a0c37f5869991ff4bbe653b8bfb
Shrutij516 <90023638+Shrutij516@users.noreply.github.com>
2024-04-27 01:08:46 +0530

    Add files via upload

---
+++ berkeley final/Clock.java
@@ -0,0 +1,14 @@
+import java.rmi.Naming;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Clock {
+	public static void main(String args[]) {
+		try {
+			ServerImpl serverImpl = new ServerImpl(LocalTime.parse(args[1], DateTimeFormatter.ofPattern("HH:mm:ss")));
+			Naming.rebind(args[0], serverImpl);
+		} catch (Exception ex) {
+			System.out.println("Execption at clock " + args[0] + ex);
+		}
+	}
+}
